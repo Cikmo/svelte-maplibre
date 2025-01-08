c999b72bfe73ca416e316995f2c39bbcc42f1bb3
Christian <contact@komodo.link>
2025-01-08 22:44:34 +0100

    update maplibre-gl to version 5.0.0 and add globe projection example

---
+++ src/routes/examples/globe_projection/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import MapLibre from '$lib/MapLibre.svelte';
+    import CodeSample from '$site/CodeSample.svelte';
+    import code from './+page.svelte?raw';
+    import { GlobeControl } from 'maplibre-gl';
+  </script>
+  
+  <MapLibre
+    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
+    standardControls
+    onload={(map) => {
+      map.addControl(new GlobeControl(), 'top-right');
+    }}
+  />
+  
+  <CodeSample
+    {code}
+    startBoundary="<MapLibre"
+    endBoundary="/>"
+    omitStartBoundary={false}
+    omitEndBoundary={false}
+  />
+  
